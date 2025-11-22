56441668753307960a5ea868c5efd8ea6cde5551
nexustech101 <charlesldefreeseiii@gmail.com>
2025-11-21 21:37:25 -0500

    Updated user account controller and included files to .gitignore

--- src/main/java/com/archtech/store/controller/UserAccountController.java
+++ src/main/java/com/archtech/store/controller/UserAccountController.java
@@ -7,11 +7,13 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:5173", "/**"})
 @RequestMapping("/api/v1/users")
 public class UserAccountController {
 
